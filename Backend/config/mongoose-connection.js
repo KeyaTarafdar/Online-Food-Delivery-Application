95b1c1ecb2534405fcf6add0ade36ac9aff0aa68
Keya Tarafdar <keya.tarafdar2003@gmail.com>
2024-09-15 03:08:48 +0530

    update

--- Backend/config/mongoose-connection.js
+++ Backend/config/mongoose-connection.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
-require('dotenv').config();
+require("dotenv").config();
 
 const mongoUri = process.env.MONGODB_URI;
 
 mongoose
-  .connect(`${mongoUri}`,{ dbName: "food_delivery" })
+  .connect(`${mongoUri}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Optional: adjust based on your needs
+  })
   .then(() => {
     dbgr("Connected");
   })
